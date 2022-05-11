738f8a1002e4725496d7990bbda1dbd69bc9fbee
Claudia Rodriguez <37720563+cloudberries27@users.noreply.github.com>
2022-05-11 16:52:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const projects=[
+    {
+        "title": "404 Page",
+        "description": "",
+        "demoLink": "",
+        "codeLink": "",
+        "tags": ["#HTML", "#CSS", "#Responsive"]
+    },
+    {
+        "title": "Team Page",
+        "description": "",
+        "demoLink": "",
+        "codeLink": "",
+        "tags": ["#HTML", "#CSS", "#Responsive"]
+    },
+    {
+        "title": "Interior Consultant",
+        "description": "",
+        "demoLink": "",
+        "codeLink": "",
+        "tags": ["#HTML", "#CSS", "#Responsive"]
+    },
+    {
+        "title": "Recipe Page",
+        "description": "",
+        "demoLink": "",
+        "codeLink": "",
+        "tags": ["#HTML", "#CSS", "#Responsive"]
+    },
+    {
+        "title": "My Gallery",
+        "description": "",
+        "demoLink": "",
+        "codeLink": "",
+        "tags": ["#HTML", "#CSS", "#Responsive"]
+    },
+    {
+        "title": "Checkout Page",
+        "description": "",
+        "demoLink": "",
+        "codeLink": "",
+        "tags": ["#HTML", "#CSS", "#Responsive"]
+    },
+    {
+        "title": "Edie Homepage",
+        "description": "",
+        "demoLink": "",
+        "codeLink": "",
+        "tags": ["#HTML", "#CSS", "#Responsive"]
+    }
+]
